85926583cad9d271d9103adf1bad1bf310595ff6
ihabberubah <37971279+ihabberubah@users.noreply.github.com>
2023-03-09 20:42:48 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+# print dasar untuk membuat text menggunakan python
+
+print ("hello, world!")
+print ("saya ingin belajar python untuk menjadi data analyst")
+print ("saya belajar python di termux menggunakan modul MICRO")
+
+# ini komentar
+# cara compile python -m py_compile (nama file)
---
+++ variabel.py
@@ -0,0 +1,49 @@
+# variabel dan value singkatnya huruf dan nilai
+
+a=5
+b=10
+c=100
+
+print (a)
+print (b)
+print (c)
+
+# untuk menambahkan text di dalam variabelnya sbb:
+
+print (a, "untuk kamu")
+print ("kalo kamu", b)
+print ("dan", c, "untuk kamu")
+
+# penulisan
+
+nilai_y=15 
+#untuk membuat space di variabel harus menggunakan underscore
+nilaiY=15.5
+#ini bisa digunakan
+angka10=1000
+#variabel bisa menggunakan angka selama angkanya tidak didepan
+
+print (nilai_y)
+print (nilaiY)
+print (angka10)
+
+#setelah pemanggilan bisa membuat variabel yang sama
+#dengan nilai/value yang berbeda. contoh:
+
+a=9
+b=19
+c=29
+
+print (a)
+print (b)
+print (c)
+
+# assignment indirect
+
+x=a
+p=b
+l=c
+
+print (x)
+print (p)
+print (l)
